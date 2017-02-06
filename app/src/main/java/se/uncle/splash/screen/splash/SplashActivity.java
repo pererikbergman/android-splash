1652414c93a6ca738ac7a5b29b34fae458c73fb1
Per-Erik Bergman <bergman@uncle.se>
2017-02-06 21:54:38 +0700

    Splash the not so right way.

--- app/src/main/java/se/uncle/splash/screen/splash/SplashActivity.java
+++ app/src/main/java/se/uncle/splash/screen/splash/SplashActivity.java
@@ -1,6 +1,7 @@
 package se.uncle.splash.screen.splash;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import se.uncle.splash.R;
@@ -8,12 +9,43 @@ import se.uncle.splash.screen.dashboard.DashboardActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASH_DELAY = 3000;
+
+    private final Handler  mHandler  = new Handler();
+    private final Launcher mLauncher = new Launcher();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
 
-        DashboardActivity.start(this);
-        finish();
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mHandler.postDelayed(mLauncher, SPLASH_DELAY);
+    }
+
+    @Override
+    protected void onStop() {
+        mHandler.removeCallbacks(mLauncher);
+
+        super.onStop();
+    }
+
+    private void launch() {
+        if (!isDestroyed()) {
+            DashboardActivity.start(SplashActivity.this);
+            finish();
+        }
+    }
+
+    private class Launcher implements Runnable {
+
+        @Override
+        public void run() {
+            launch();
+        }
     }
 }
